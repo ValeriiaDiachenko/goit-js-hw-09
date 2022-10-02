5952768a4258e54da62be74f54f4709d7a54e47c
Valeriia Diachenko <sanjana.samiyya@gmail.com>
2022-10-02 20:35:34 +0300

    Task1 update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,35 +1,24 @@
-const refs = {
-  start: document.querySelector('[data-start]'),
-  stop: document.querySelector('[data-stop]'),
-  body: document.body,
-};
+const start = document.querySelector('[data-start]');
+const stop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
 
-let intervalID = null;
+start.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    const color = getRandomHexColor();
+    body.style.backgroundColor = color;
+  }, 1000);
+  start.disabled = true;
+  stop.disabled = false;
+});
 
-refs.start.addEventListener('click', onStartBtnClick);
-refs.stop.addEventListener('click', onStopBtnClick);
-
-function onStartBtnClick() {
-  intervalID = setInterval(changeBodyBgColor, 1000);
-
-  switchBtns();
-}
-
-function onStopBtnClick() {
-  clearInterval(intervalID);
-  switchBtns();
-}
-
-function changeBodyBgColor() {
-  refs.body.style.backgroundColor = getRandomHexColor();
-}
+stop.addEventListener('click', () => {
+  clearInterval(timerId);
+  start.disabled = false;
+  stop.disabled = true;
+});
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-
-function switchBtns() {
-  refs.start.disabled = !refs.start.disabled;
-  refs.stop.disabled = !refs.stop.disabled;
 }
 
